ec196cca1bed40e203f7b902c837c01886924ca8
jeonhyeonseon <goodpretty0325@gmail.com>
2024-08-03 16:46:39 +0900

    init: chat entity 생성 #41

--- src/main/java/com/up/and/down/chatroom/entity/Chat.java
+++ src/main/java/com/up/and/down/chatroom/entity/Chat.java
@@ -1,4 +1,25 @@
 package com.up.and.down.chatroom.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tbl_chat")
+@Data
+@Setter(AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
 public class Chat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long chatRoomId; // 채팅방 id(fk)
+    private Long memberId; // 채팅 멤버 id(fk)
+    private String content; // 메시지 내용
+    @CreatedDate
+    private LocalDateTime createdAt; // 생성시간
 }
