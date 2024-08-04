210bfe12ad2e5a2e1bad34c736b5ddc8b81b7a70
jeonhyeonseon <goodpretty0325@gmail.com>
2024-08-04 20:44:00 +0900

    Init(src/main/java/com/up/and/down/chatroom/entity/Chat): chat aggregate 추가 #41

--- src/main/java/com/up/and/down/chatroom/entity/Chat.java
+++ src/main/java/com/up/and/down/chatroom/entity/Chat.java
@@ -5,6 +5,7 @@ import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Entity
 @Table(name = "tbl_chat")
@@ -17,8 +18,23 @@ public class Chat {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long chatRoomId; // 채팅방 id(fk)
-    private Long memberId; // 채팅 멤버 id(fk)
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(
+            name = "tbl_chat_chatroom",
+            joinColumns = @JoinColumn(name = "chat_id")
+    )
+    @Column(name = "chat_room_id")
+    private Set<Long> chatRoomId; // 채팅방 id(fk)
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(
+            name = "tbl_chat_members",
+            joinColumns = @JoinColumn(name = "chat_id")
+    )
+    @Column(name = "member_id")
+    private Set<Long> memberId; // 채팅 멤버 id(fk)
+
     private String content; // 메시지 내용
     @CreatedDate
     private LocalDateTime createdAt; // 생성시간
