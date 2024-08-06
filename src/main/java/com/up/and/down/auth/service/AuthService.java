17fa69dd898580971002c58fc962edeff9d21427
subkka <beeniaa@naver.com>
2024-08-06 10:15:56 +0900

    Update(src/main/java/com/up/and/down): entity 수정 #13

--- src/main/java/com/up/and/down/auth/service/AuthService.java
+++ src/main/java/com/up/and/down/auth/service/AuthService.java
@@ -1,4 +1,26 @@
 package com.up.and.down.auth.service;
 
-public class AuthService {
+import com.up.and.down.auth.principal.AuthPrincipal;
+import com.up.and.down.user.User;
+import com.up.and.down.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class AuthService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findById(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        return new AuthPrincipal(user); // @Data가 @AllArgsConstructor를 같이 선언해주기 때문에 생성자 사용이 가능
+    }
 }
--- src/main/java/com/up/and/down/user/admin/entity/Admin.java
+++ src/main/java/com/up/and/down/user/admin/entity/Admin.java
@@ -1,6 +1,7 @@
 package com.up.and.down.user.admin.entity;
 
 import com.up.and.down.user.User;
+import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -16,9 +17,21 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class Admin extends User {
+    @Column(nullable = false, unique = true)
     private String empNum; // 사번
     private Department department; // 소속 부서
     private Hierarchy hierarchy; // 직급
+    @Column(name = "office_number", nullable = true)
     private String officeNumber; // 사내 유선전화번호
     private EmpStatus empStatus; // 재직여부
+
+    @Override
+    public String getUserId() {
+        return empNum;
+    }
+
+    @Override
+    public void setUserId(String userId) {
+        throw new UnsupportedOperationException("Admin does not use userId");
+    }
 }
