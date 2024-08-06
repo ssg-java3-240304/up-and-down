b9ffe5e371f9be1be44ac7ae686a1cc7ddd4780d
jeonhyeonseon <goodpretty0325@gmail.com>
2024-08-06 19:54:12 +0900

    Update(controller/ChatStompController): 엔드포인스 수정2 #91

--- src/main/java/com/up/and/down/chatroom/controller/ChatStompController.java
+++ src/main/java/com/up/and/down/chatroom/controller/ChatStompController.java
@@ -1,17 +1,14 @@
 package com.up.and.down.chatroom.controller;
 
 import com.up.and.down.chatroom.dto.ChatDto;
-import com.up.and.down.chatroom.entity.Chat;
 import com.up.and.down.chatroom.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
-import java.time.LocalDateTime;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,9 +16,9 @@ import java.time.LocalDateTime;
 public class ChatStompController {
     private final ChatService chatService;
 
-    @MessageMapping("{chatRoomId}")
-    @SendTo("/sub/{chatRoomId}")
-    public ChatDto chat(@DestinationVariable(value = "chatroomId") Long chatroomId, ChatDto chatDto){
+    @MessageMapping("/chat/chatroom/{roomId}")
+    @SendTo("/sub/chat/chatroom/{roomId}")
+    public ChatDto chat(@DestinationVariable(value = "roomId") Long chatroomId, ChatDto chatDto){
         log.debug("chatroomId = {}", chatroomId);
         log.debug("chatDto = {}", chatDto);
         chatService.saveMessage(chatDto); // 메시지 db에 저장
