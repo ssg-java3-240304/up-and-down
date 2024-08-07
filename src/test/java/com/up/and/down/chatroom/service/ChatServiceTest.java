e31b9a909e75f3b7d345231a1e532ba8bb04e588
jeonhyeonseon <goodpretty0325@gmail.com>
2024-08-07 11:53:06 +0900

    Update(src/test/java/com/up/and/down/chatroom/service/ChatServiceTest): 채팅 메시지 불러오기 #89

--- src/test/java/com/up/and/down/chatroom/service/ChatServiceTest.java
+++ src/test/java/com/up/and/down/chatroom/service/ChatServiceTest.java
@@ -11,59 +11,20 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @SpringBootTest
 @Transactional
 class ChatServiceTest {
     @Autowired
-    private ChatService chatService;
-    @Autowired
     private ChatRepository chatRepository;
 
     @Test
-    @DisplayName("메시지 등록")
-    void test1(){
+    @DisplayName("채팅 메시지 불러오기")
+    void findChatMessageByChatRoomId() {
         // given
-        ChatDto chatDto = new ChatDto();
-        chatDto.setChatRoomId(1L);
-        chatDto.setNickname("야호");
-        chatDto.setMessage("안녕하세여. 야호입니다");
+        List<Chat> chatList = chatRepository.findChatMessageByChatRoomId(1L);
         // when
-        Chat savedChat = chatService.saveMessage(chatDto);
+        List<ChatDto> chatDtos = chatList.stream().map(ChatDto::toChatDto).toList();
         // then
-        assertThat(savedChat.getId()).isNotNull();
-        assertThat(savedChat.getChatRoomId()).isEqualTo(1L);
-        assertThat(savedChat.getNickname()).isEqualTo("야호");
-        assertThat(savedChat.getMessage()).isEqualTo("안녕하세여. 야호입니다");
-        // 실제로 db에 저장되었는 지 확인해보기
-        Chat foundChat = chatRepository.findById(savedChat.getId()).orElse(null);
-        assertThat(foundChat).isNotNull();
-        assertThat(foundChat.getChatRoomId()).isEqualTo(1L);
-        assertThat(foundChat.getNickname()).isEqualTo("야호");
-        assertThat(foundChat.getMessage()).isEqualTo("안녕하세여. 야호입니다");
-    }
-
-    @Test
-    @DisplayName("메시지 내역 불러오기")
-    void test2() {
-        // given
-        ChatDto chatDto1 = new ChatDto();
-        chatDto1.setChatRoomId(1L);
-        chatDto1.setNickname("전");
-        chatDto1.setMessage("현선입니다");
-
-        ChatDto chatDto2 = new ChatDto();
-        chatDto2.setChatRoomId(1L);
-        chatDto2.setNickname("야");
-        chatDto2.setMessage("호입니다");
-
-        chatService.saveMessage(chatDto1);
-        chatService.saveMessage(chatDto2);
-        // when
-        List<ChatDto> chatList = chatService.findChatMessageByChatRoomId(1L);
-        // then
-        assertThat(chatList).hasSize(2);
-        assertThat(chatList).extracting(ChatDto::getMessage).containsExactly("현선입니다", "호입니다");
+        System.out.println(chatDtos);
     }
 }
