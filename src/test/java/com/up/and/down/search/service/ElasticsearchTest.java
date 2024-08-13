08009d7214984b6ac981e7f951e00c97bc16e1db
hurryduck <tigeryck0812@gmail.com>
2024-08-13 16:31:16 +0900

    Rename: elasticsearch, database 테스트 케이스 분리 #98

--- src/test/java/com/up/and/down/search/service/ElasticsearchTest.java
+++ src/test/java/com/up/and/down/search/service/ElasticsearchTest.java
@@ -15,7 +15,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest()
-class SearchServiceTest {
+class ElasticsearchTest {
     @Autowired
     private ProductGroupDocRepository repo;
 
