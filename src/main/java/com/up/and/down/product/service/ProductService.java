05059f3b844249279fb96d5d6fbbbc98e06f972c
hurryduck <tigeryck0812@gmail.com>
2024-08-22 15:13:25 +0900

    feat: 상품 이미지 노출 #119
    1. webSecurity 경로 추가
    2. index.html 실시간 최다 검색 이미지 테두리 라운드 추가
    3. product.html 상품 이미지 노출
    4. 올바르지 않은 접근 차단

--- src/main/java/com/up/and/down/product/service/ProductService.java
+++ src/main/java/com/up/and/down/product/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.up.and.down.product.service;
 
+import com.up.and.down.product.entity.ProductGroup;
+import com.up.and.down.product.repository.ProductGroupRepository;
 import com.up.and.down.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -7,6 +9,10 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class ProductService {
-    private final ProductRepository productRepo;
-    private final ProductRepository productGroupRepo;
+    private final ProductGroupRepository productGroupRepo;
+
+
+    public ProductGroup findById(Long productGroupId) {
+        return productGroupRepo.findById(productGroupId).orElse(null);
+    }
 }
