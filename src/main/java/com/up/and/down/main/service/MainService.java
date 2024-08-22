fa7e6ac22023d523f75eeb0eb108bfba5f615fce
hurryduck <tigeryck0812@gmail.com>
2024-08-22 10:20:41 +0900

    hotfix: 로그 추가

--- src/main/java/com/up/and/down/main/service/MainService.java
+++ src/main/java/com/up/and/down/main/service/MainService.java
@@ -4,11 +4,13 @@ import com.up.and.down.product.entity.ProductGroup;
 import com.up.and.down.search.repository.ProductGroupDocRepository;
 import com.up.and.down.search.service.ProductGroupToEntityService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MainService {
@@ -16,7 +18,10 @@ public class MainService {
     private final ProductGroupToEntityService productGroupToEntityService;
 
     public List<ProductGroup> findTop4ByOrderByViewCountDesc() {
-        this.repo.findTop4ByOrderByViewCountDesc().forEach(System.out::println);
+        log.info("findTop4ByOrderByViewCountDesc start");
+        this.repo.findTop4ByOrderByViewCountDesc().forEach(productGroupDoc -> {
+            log.debug("productGroupDoc productListJson: {}", productGroupDoc.getProductListJson());
+        });
         return this.repo.findTop4ByOrderByViewCountDesc().stream()
                 .map(this.productGroupToEntityService::toEntity)
                 .collect(Collectors.toList());
