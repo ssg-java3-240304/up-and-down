72be891044fb186e5bb69bb30b002eca30591e1b
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2024-08-26 10:44:22 +0900

    feat : 테마 변경

--- src/main/java/com/up/and/down/chatroom/entity/Category.java
+++ src/main/java/com/up/and/down/chatroom/entity/Category.java
@@ -3,14 +3,17 @@ package com.up.and.down.chatroom.entity;
 import lombok.Getter;
 @Getter
 public enum Category {
-    HIKING("등산"),
-    WALKING("산책"),
-    CAMPING("캠핑"),
-    FESTIVAL("축제"),
-    BEACH("바다"),
-    HEALING("힐링"),
-    FISHING("낚시"),
-    OTHERS("기타");
+    ADVENTURE( "모험"),
+    RELAXATION("휴식"),
+    CULTURE("문화"),
+    NATURE( "자연"),
+    FOOD( "맛집"),
+    HISTORY("역사"),
+    LUXURY( "럭셔리"),
+    FAMILY( "가족"),
+    ROMANTIC( "로맨틱"),
+    SHOPPING( "쇼핑"),
+    FESTIVAL( "축제");
 
     private final String displayName;
 
