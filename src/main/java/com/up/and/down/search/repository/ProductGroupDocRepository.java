25ffe18f3877590242e110cc3ad3eaad2f6ec755
hurryduck <tigeryck0812@gmail.com>
2024-08-26 21:42:20 +0900

    test: 불필요 테스트 코드 삭제

--- src/main/java/com/up/and/down/search/repository/ProductGroupDocRepository.java
+++ src/main/java/com/up/and/down/search/repository/ProductGroupDocRepository.java
@@ -1,5 +1,6 @@
 package com.up.and.down.search.repository;
 
+import com.up.and.down.product.entity.Destination;
 import com.up.and.down.product.entity.ProductGroup;
 import com.up.and.down.search.entity.ProductGroupDoc;
 import lombok.NonNull;
@@ -20,37 +21,11 @@ public interface ProductGroupDocRepository extends ElasticsearchRepository<Produ
 
     List<ProductGroupDoc> findByNights(int nights); // 숙박일로 검색
 
-    List<ProductGroupDoc> findByStartDate(LocalDate startDate); // 여행출발일로 검색
-
-    List<ProductGroupDoc> findByStartDateAfter(LocalDate startDate); // 여행 출발일 이후 검색
-
     List<ProductGroupDoc> findBySearchKeywordsAndNights(String searchKeyword, int nights); // 키워드, 숙박일
-
-    List<ProductGroupDoc> findBySearchKeywordsAndStartDateAfter(String searchKeyword, LocalDate startDate); // 키워드, 여행 출발일 이후
-
-    List<ProductGroupDoc> findByNightsAndStartDateAfter(int nights, LocalDate startDate); // 숙박일, 여행 출발일 이후
-
-    List<ProductGroupDoc> findBySearchKeywordsAndNightsAndStartDateAfter(String searchKeyword, int nights, LocalDate startDate); // 키워드, 숙박일, 여행 출발일
-
-    List<ProductGroupDoc> findAllByOrderByViewCountDesc(); // 조회수 순 검색
 
     List<ProductGroupDoc> findTop4ByOrderByViewCountDesc(); // 조회수 순 상위 4개
 
     // 페이지네이션
-    Page<ProductGroupDoc> findBySearchKeywords(String searchKeyword, Pageable pageable);
-
-    Page<ProductGroupDoc> findByNights(int nights, Pageable pageable);
-
-    Page<ProductGroupDoc> findByStartDateAfter(LocalDate startDate, Pageable pageable);
-
-    Page<ProductGroupDoc> findBySearchKeywordsAndNights(String searchKeyword, int nights, Pageable pageable);
-
-    Page<ProductGroupDoc> findBySearchKeywordsAndNightsAndStartDateAfter(String searchKeyword, int nights, LocalDate startDate, Pageable pageable);
-
-    Page<ProductGroupDoc> findByNightsAndStartDateAfter(int nights, LocalDate startDate, Pageable pageable);
-
-    Page<ProductGroupDoc> findBySearchKeywordsAndStartDateAfter(String searchKeyword, LocalDate startDate, Pageable pageable);
-
     // 페이지네이션: 조회수순으로 조회
     Page<ProductGroupDoc> findBySearchKeywordsOrderByViewCountDesc(String searchKeyword, Pageable pageable);
 
@@ -85,5 +60,4 @@ public interface ProductGroupDocRepository extends ElasticsearchRepository<Produ
     List<ProductGroupDoc> findTop4ByNightsOrderByViewCountDesc(int nights);
 
     List<ProductGroupDoc> findTop4BySearchKeywordsOrderByLikeCountDesc(String theme);
-
 }
