80a2b7fb0770e0630fb20170ed15bdab27bcb1dd
hurryduck <tigeryck0812@gmail.com>
2024-08-27 12:47:19 +0900

    update: 도메인 변경

--- src/main/java/com/up/and/down/config/webSecurity/WebSecurityConfig.java
+++ src/main/java/com/up/and/down/config/webSecurity/WebSecurityConfig.java
@@ -38,20 +38,26 @@ public class WebSecurityConfig {
          */
         http.authorizeHttpRequests((registry) -> {
             registry
-                    .requestMatchers(
+                    .requestMatchers( // permitAll
                             "/",
                             "/search/**",
                             "/product/**",
-                            "/chat-rooms/chat",
+                            "/chatroom/chat",
+                            "/chatroom/list",
                             "/chat/**",
-                            "/api/public",
                             "/member/**",
                             "/sns_api",
-                            "/chat-rooms/list",
                             "/admin/dashboard",
                             "/admin/stat/**"
-                    ).permitAll() // 누구나 허용
-                    .requestMatchers("/auth/login/**", "/join/**", "/admin/login", "/admin/register", "/admin/sign/send", "/admin/check").anonymous()
+                    ).permitAll()
+                    .requestMatchers( // anonymous
+                            "/auth/login/**",
+                            "/join/**",
+                            "/admin/login",
+                            "/admin/register",
+                            "/admin/sign/send",
+                            "/admin/check"
+                    ).anonymous()
                     .requestMatchers("/admin/**").hasRole("ADMIN") // ROLE_ADMIN 권한이 있는 사용자만 허용
                     .anyRequest().authenticated();
         });
